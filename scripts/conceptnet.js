c57ca263e9ae3ef65e695038b165270bdec01420
kengz <kengzwl@gmail.com>
2016-03-20 15:31:10 -0400

    update conceptnet algo outline; need nlp next

--- scripts/conceptnet.js
+++ scripts/conceptnet.js
@@ -4,8 +4,11 @@ var cn = ConceptNet(null, null, '5.4')
 // refer: https://github.com/commonsense/conceptnet5/wiki
 // https://github.com/commonsense/conceptnet5/wiki/API
 // algo to convert input to parameter:
+// 0. higher level NLP interface for this KB
 // 1. determine language
 // 2. need uri standardization
+// 3. determine parameters, and what graph user looks for: either just concept, or assertations, rel. See URI hierarchy https://github.com/commonsense/conceptnet5/wiki/URI-hierarchy
+// 4. output result parser? e.g. compose into NL sentence
 // 
 // examples
 
