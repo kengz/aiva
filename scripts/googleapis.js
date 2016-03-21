0102a1d0cbfe904761ea7583b4dfe07581e406f9
kengz <kengzwl@gmail.com>
2016-03-20 23:33:20 -0400

    add google search scraper

--- scripts/googleapis.js
+++ scripts/googleapis.js
@@ -71,3 +71,27 @@ var urlshortener = google.urlshortener({
 
 // // this below need to use an oauth2 client
 // urlshortener.url.list({}, logCb)
+
+
+// Google search via scraper: 
+// https://www.npmjs.com/package/google
+var google = require('google')
+google.resultsPerPage = 25
+
+
+var nextCounter = 0
+ 
+google('node.js best practices', function (err, res){
+  if (err) console.error(err)
+ 
+  for (var i = 0; i < res.links.length; ++i) {
+    var link = res.links[i];
+    console.log(link.title + ' - ' + link.href)
+    console.log(link.description + "\n")
+  }
+ 
+  if (nextCounter < 4) {
+    nextCounter += 1
+    if (res.next) res.next()
+  }
+})
