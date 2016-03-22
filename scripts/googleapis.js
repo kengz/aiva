eacbe4cfdd5d4ee7a2915e069ad2b74e07b0276e
kengz <kengzwl@gmail.com>
2016-03-21 23:19:02 -0400

    mute google apis and natural for tests

--- scripts/googleapis.js
+++ scripts/googleapis.js
@@ -1,97 +1,97 @@
-var google = require('googleapis')
+// var google = require('googleapis')
 
-// List of APIs supported by google api node client: https://developers.google.com/apis-explorer/#p/
-// Get key: 
-// Create a Google Developer account https://console.developers.google.com/
-// Create a project, then generate an API key (server key) https://console.developers.google.com/apis/credentials
-// Enable the APIs you wish to use https://console.developers.google.com/apis/enabled
-// moreover you may with to set a consent screen and create OAuth credentials for certain APIs that's that reaches into your private information, e.g. gmail API
-// (provide the list of APIs needed to be eneabled):
-// - Calendar API
-// - !Cloud Vision API
-// - !CustomSearch API
-// - Drive API
-// - !Knowledge Graph Search API 
-// - !Maps (multiple) APIs
-// - Prediction API
-// - QPX Express API
-// - Translate API
-// - !URL shortener
-// - YouTube Data API v3
-// - Google Voice (separate for android) https://developers.google.com/voice-actions/system/#step_1_define_an_intent_filter
-
-
-// References
-// 
-// docs: 
-// http://google.github.io/google-api-nodejs-client/3.1.0/index.html
-// API explorer
-// https://developers.google.com/apis-explorer
-// API list
-// https://developers.google.com/apis-explorer/#p/
-// for 'login required', need oauth: 
-// https://github.com/google/google-api-nodejs-client/issues/398
-// https://developers.google.com/gmail/api/quickstart/nodejs#step_3_set_up_the_sample
-// oauth prob:
-// https://github.com/google/google-api-nodejs-client/issues/398
+// // List of APIs supported by google api node client: https://developers.google.com/apis-explorer/#p/
+// // Get key: 
+// // Create a Google Developer account https://console.developers.google.com/
+// // Create a project, then generate an API key (server key) https://console.developers.google.com/apis/credentials
+// // Enable the APIs you wish to use https://console.developers.google.com/apis/enabled
+// // moreover you may with to set a consent screen and create OAuth credentials for certain APIs that's that reaches into your private information, e.g. gmail API
+// // (provide the list of APIs needed to be eneabled):
+// // - Calendar API
+// // - !Cloud Vision API
+// // - !CustomSearch API
+// // - Drive API
+// // - !Knowledge Graph Search API 
+// // - !Maps (multiple) APIs
+// // - Prediction API
+// // - QPX Express API
+// // - Translate API
+// // - !URL shortener
+// // - YouTube Data API v3
+// // - Google Voice (separate for android) https://developers.google.com/voice-actions/system/#step_1_define_an_intent_filter
 
 
-// test code
-var env = require('node-env-file');
-env(__dirname + '/../bin/.keys-aiva', { overwrite: false });
-var auth = process.env.GOOGLE_API_KEY;
-var oauth = process.env.GOOGLE_OAUTH_CLIENT;
-// console.log(auth)
+// // References
+// // 
+// // docs: 
+// // http://google.github.io/google-api-nodejs-client/3.1.0/index.html
+// // API explorer
+// // https://developers.google.com/apis-explorer
+// // API list
+// // https://developers.google.com/apis-explorer/#p/
+// // for 'login required', need oauth: 
+// // https://github.com/google/google-api-nodejs-client/issues/398
+// // https://developers.google.com/gmail/api/quickstart/nodejs#step_3_set_up_the_sample
+// // oauth prob:
+// // https://github.com/google/google-api-nodejs-client/issues/398
 
-// helper
-function logCb(err, res) {
-  if (err) {
-    console.log('Encountered error', err);
-  } else {
-    console.log('Response:', res);
-  }
-}
 
-var urlshortener = google.urlshortener({
-  version: 'v1',
-  auth: auth
-});
+// // test code
+// var env = require('node-env-file');
+// env(__dirname + '/../bin/.keys-aiva', { overwrite: false });
+// var auth = process.env.GOOGLE_API_KEY;
+// var oauth = process.env.GOOGLE_OAUTH_CLIENT;
+// // console.log(auth)
 
-// // get the long url of a shortened url
-// var params = { shortUrl: 'http://goo.gl/xKbRu3' };
-// urlshortener.url.get(params, logCb);
-
-// Creates a new short URL
-// var params = {
-//   resource: {
-//     longUrl: 'http://google.github.io/google-api-nodejs-client/3.1.0/urlshortener.html'
+// // helper
+// function logCb(err, res) {
+//   if (err) {
+//     console.log('Encountered error', err);
+//   } else {
+//     console.log('Response:', res);
 //   }
-// };
-// urlshortener.url.insert(params, logCb)
+// }
 
-// // this below need to use an oauth2 client
-// urlshortener.url.list({}, logCb)
+// var urlshortener = google.urlshortener({
+//   version: 'v1',
+//   auth: auth
+// });
+
+// // // get the long url of a shortened url
+// // var params = { shortUrl: 'http://goo.gl/xKbRu3' };
+// // urlshortener.url.get(params, logCb);
+
+// // Creates a new short URL
+// // var params = {
+// //   resource: {
+// //     longUrl: 'http://google.github.io/google-api-nodejs-client/3.1.0/urlshortener.html'
+// //   }
+// // };
+// // urlshortener.url.insert(params, logCb)
+
+// // // this below need to use an oauth2 client
+// // urlshortener.url.list({}, logCb)
 
 
-// Google search via scraper: 
-// https://www.npmjs.com/package/google
-var google = require('google')
-google.resultsPerPage = 25
+// // Google search via scraper: 
+// // https://www.npmjs.com/package/google
+// var google = require('google')
+// google.resultsPerPage = 25
 
 
-var nextCounter = 0
+// var nextCounter = 0
  
-google('node.js best practices', function (err, res){
-  if (err) console.error(err)
+// google('node.js best practices', function (err, res){
+//   if (err) console.error(err)
  
-  for (var i = 0; i < res.links.length; ++i) {
-    var link = res.links[i];
-    console.log(link.title + ' - ' + link.href)
-    console.log(link.description + "\n")
-  }
+//   for (var i = 0; i < res.links.length; ++i) {
+//     var link = res.links[i];
+//     console.log(link.title + ' - ' + link.href)
+//     console.log(link.description + "\n")
+//   }
  
-  if (nextCounter < 4) {
-    nextCounter += 1
-    if (res.next) res.next()
-  }
-})
+//   if (nextCounter < 4) {
+//     nextCounter += 1
+//     if (res.next) res.next()
+//   }
+// })
--- scripts/natural.js
+++ scripts/natural.js
@@ -1,46 +1,46 @@
-var natural = require('natural');
-var wordnet = new natural.WordNet();
+// var natural = require('natural');
+// var wordnet = new natural.WordNet();
 
-// Design:
-// Use the more superior https://github.com/moos/wordpos
-// parse POS into intent, args? not so simple but it's a step forward
+// // Design:
+// // Use the more superior https://github.com/moos/wordpos
+// // parse POS into intent, args? not so simple but it's a step forward
 
-wordnet.lookup('node', function(results) {
-    results.forEach(function(result) {
-        console.log('------------------------------------');
-        console.log(result.synsetOffset);
-        console.log(result.pos);
-        console.log(result.lemma);
-        console.log(result.synonyms);
-        console.log(result.pos);
-        console.log(result.gloss);
-    });
-});
+// wordnet.lookup('node', function(results) {
+//     results.forEach(function(result) {
+//         console.log('------------------------------------');
+//         console.log(result.synsetOffset);
+//         console.log(result.pos);
+//         console.log(result.lemma);
+//         console.log(result.synonyms);
+//         console.log(result.pos);
+//         console.log(result.gloss);
+//     });
+// });
 
 
-// Better wordpos
-var WordPOS = require('wordpos'),
-    wordpos = new WordPOS();
+// // Better wordpos
+// var WordPOS = require('wordpos'),
+//     wordpos = new WordPOS();
 
-wordpos.getAdjectives('The angry bear chased the frightened little squirrel.', function(result){
-    console.log(result);
-});
-// [ 'little', 'angry', 'frightened' ]
+// wordpos.getAdjectives('The angry bear chased the frightened little squirrel.', function(result){
+//     console.log(result);
+// });
+// // [ 'little', 'angry', 'frightened' ]
 
-wordpos.isAdjective('awesome', function(result){
-    console.log(result);
-});
-// true 'awesome'
+// wordpos.isAdjective('awesome', function(result){
+//     console.log(result);
+// });
+// // true 'awesome'
 
-wordpos.getNouns('The angry bear chased the frightened little squirrel.', console.log)
-// [ 'bear', 'squirrel', 'little', 'chased' ]
+// wordpos.getNouns('The angry bear chased the frightened little squirrel.', console.log)
+// // [ 'bear', 'squirrel', 'little', 'chased' ]
 
-wordpos.getPOS('The angry bear chased the frightened little squirrel.', console.log)
-// output:
-  // {
-  //   nouns: [ 'bear', 'squirrel', 'little', 'chased' ],
-  //   verbs: [ 'bear' ],
-  //   adjectives: [ 'little', 'angry', 'frightened' ],
-  //   adverbs: [ 'little' ],
-  //   rest: [ 'the' ]
-  // }
+// wordpos.getPOS('The angry bear chased the frightened little squirrel.', console.log)
+// // output:
+//   // {
+//   //   nouns: [ 'bear', 'squirrel', 'little', 'chased' ],
+//   //   verbs: [ 'bear' ],
+//   //   adjectives: [ 'little', 'angry', 'frightened' ],
+//   //   adverbs: [ 'little' ],
+//   //   rest: [ 'the' ]
+//   // }
