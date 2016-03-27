956bb88dde9b4ec2e8feae416f8f10d048dd5249
kengz <kengzwl@gmail.com>
2016-03-27 16:21:32 -0400

    finish titanic deployment example

--- lib/py/ai/dnn_1_deploy.py
+++ lib/py/ai/dnn_1_deploy.py
@@ -1,22 +1,28 @@
-from sklearn import datasets, metrics, cross_validation
+import numpy as np
+from ai_lib import preprocess
+from sklearn import cross_validation, metrics
+import pandas
 import skflow
-import os.path
 
 # reliable absolute path when this module is called elsewhere
-data_path = os.path.join(os.path.dirname(__file__), 'data/titanic.csv')
-model_path = os.path.join(os.path.dirname(__file__), 'models/iris_dnn')
-
-import numpy as np
+data_path = preprocess.abspath('data/titanic.csv')
+model_path = preprocess.abspath('models/titanic_dnn')
 
 # load the dataset
-# train = np.genfromtxt()
-iris = datasets.load_iris()
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(iris.data, iris.target,
-    test_size=0.2, random_state=42)
+df = pandas.read_csv(data_path)
+X, y = df[['Sex', 'Age', 'SibSp', 'Fare']], df['Survived']
+# chain: fillna for 'Sex' with 'NA', the rest with 0
+X = X.fillna({'Sex': 'NA'}).fillna(0)
+# Label Encoder to encode string entries into integers
+le_X = preprocess.MultiColumnLabelEncoder(columns = ['Sex'])
+X = le_X.fit_transform(X)
 
-# iris model
+# random-split into train (80%), test data (20%)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state=42)
+
+# model
 classifier = skflow.TensorFlowEstimator.restore(model_path)
-print('Model loaded from models/iris_dnn')
+print('Model loaded from', model_path)
 
 
 # Methods exports for usage
@@ -39,6 +45,5 @@ def train(X, y):
   return 'Feature to be implemented, waiting for skflow team'
 
 
-# print(predict([[ 5.1,3.5,1.4,0.2]]))
-# print(predict([ 5.1,3.5,1.4,0.2]))
-# accuracy()
+# print(X_test[:1], predict(X_test[:1]))
+# print(accuracy())
--- lib/py/ai/dnn_1_train.py
+++ lib/py/ai/dnn_1_train.py
@@ -1,41 +1,46 @@
 # *_train.py is for offline training, shall not be run during deployment
 import numpy as np
-from ai_lib import * # preprocess helpers
-from sklearn import pipeline, cross_validation, metrics
+from ai_lib import preprocess
+from sklearn import cross_validation, metrics
 import pandas
 import skflow
 
-# reliable absolute path when this module is called elsewhere
-data_path = preprocess.abspath('data/titanic.csv')
-model_path = preprocess.abspath('models/titanic_dnn')
+# method wrap for safety, so training doesn't run when imported by accident
+def train():
+  # reliable absolute path when this module is called elsewhere
+  data_path = preprocess.abspath('data/titanic.csv')
+  model_path = preprocess.abspath('models/titanic_dnn')
 
-# load and clean the dataset
-train = pandas.read_csv(data_path)
+  # load and clean the dataset
+  df = pandas.read_csv(data_path)
+  X, y = df[['Sex', 'Age', 'SibSp', 'Fare']], df['Survived']
+  # chain: fillna for 'Sex' with 'NA', the rest with 0
+  X = X.fillna({'Sex': 'NA'}).fillna(0)
+  # Label Encoder to encode string entries into integers
+  le_X = preprocess.MultiColumnLabelEncoder(columns = ['Sex'])
+  X = le_X.fit_transform(X)
 
-X, y = train[['Sex', 'Age', 'SibSp', 'Fare']], train['Survived']
-# chain: fillna for 'Sex' with 'NA', the rest with 0
-X = X.fillna({'Sex': 'NA'}).fillna(0)
+  # random-split into train (80%), test data (20%)
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Label Encoder to encode string entries into integers
-le_X = preprocess.MultiColumnLabelEncoder(columns = ['Sex'])
-X = le_X.fit_transform(X)
+  # Build 3 layer DNN with 10, 20, 10 units respecitvely.
+  classifier = skflow.TensorFlowDNNClassifier(
+    hidden_units=[10, 20, 10],
+    n_classes=2,
+    steps=500,
+    learning_rate=0.01
+  )
 
-# random-split into train (80%), test data (20%)
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state=42)
+  # Fit and save model for deployment.
+  classifier.fit(X_train, y_train)
+  score = metrics.accuracy_score(y_test, classifier.predict(X_test))
+  print('Accuracy: {0:f}'.format(score))
+  # should be arond 0.74
 
-# Build 3 layer DNN with 10, 20, 10 units respecitvely.
-classifier = skflow.TensorFlowDNNClassifier(
-  hidden_units=[10, 20, 10],
-  n_classes=2,
-  steps=500,
-  learning_rate=0.01
-)
+  # save the model for use
+  classifier.save(model_path)
+  print('Model saved to', model_path)
 
-# Fit and save model for deployment.
-classifier.fit(X_train, y_train)
-score = metrics.accuracy_score(y_test, classifier.predict(X_test))
-print('Accuracy: {0:f}'.format(score))
 
-# save the model for use
-classifier.save(model_path)
-print('Model saved to', model_path)
+# Uncomment to train it
+# train()
