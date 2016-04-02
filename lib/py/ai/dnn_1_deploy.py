d8f0f8c5f482b9284b0c030c491e41840df80e0f
kengz <kengzwl@gmail.com>
2016-04-02 02:55:47 -0400

    wrap in init with global vars for control

--- lib/py/ai/dnn_1_deploy.py
+++ lib/py/ai/dnn_1_deploy.py
@@ -6,14 +6,17 @@ import skflow
 
 # reliable absolute path when this module is called elsewhere
 model_path = preprocess.abspath('models/titanic_dnn')
-print('model_path', model_path)
+classifier = None
+mle = None
 
-# load the saved model and label encoder
-classifier = skflow.TensorFlowEstimator.restore(model_path)
-mle = preprocess.MultiLabelEncoder().restore(model_path)
-print('Model loaded from', model_path, mle)
 
-# export methods for usage:
+def init():
+  global classifier, mle
+  # load the saved model and label encoder
+  classifier = skflow.TensorFlowEstimator.restore(model_path)
+  mle = preprocess.MultiLabelEncoder().restore(model_path)
+  print('Model loaded from', model_path)
+  return classifier
 
 # Get the accuracy of the current model
 def accuracy():
@@ -48,24 +51,24 @@ def train(X, y, save=False):
   return classifier
 
 
+# # Example usage
+# # make a prediction
+# init()
+# x = ['male', 22, 1, 7.25]
+# print(x, predict(x))
 
-# Example usage
-# make a prediction
-x = ['male', 22, 1, 7.25]
-print(x, predict(x))
+# # continue training with more data set to 'improve'
+# data_path = preprocess.abspath('data/titanic.csv')
+# df = pandas.read_csv(data_path)
+# X, y = df[['Sex', 'Age', 'SibSp', 'Fare']], df['Survived']
+# # chain: fillna for str with 'NA', num with 0, transform
+# X = preprocess.MultiFillna(X)
+# X = mle.fit_transform(X)
+# # random-split into train (80%), test data (20%)
+# X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state=42)
 
-# continue training with more data set to 'improve'
-data_path = preprocess.abspath('data/titanic.csv')
-df = pandas.read_csv(data_path)
-X, y = df[['Sex', 'Age', 'SibSp', 'Fare']], df['Survived']
-# chain: fillna for str with 'NA', num with 0, transform
-X = preprocess.MultiFillna(X)
-X = mle.fit_transform(X)
-# random-split into train (80%), test data (20%)
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state=42)
-
-print(X_test[:1], predict(X_test[:1]))
-# current model accuracy, about 0.74
-print(accuracy())
-# cheating: train more on test data, accuracy should increase, about 0.76
-train(X_test, y_test, save=False)
+# print(X_test[:1], predict(X_test[:1]))
+# # current model accuracy, about 0.74
+# print(accuracy())
+# # cheating: train more on test data, accuracy should increase, about 0.76
+# train(X_test, y_test, save=False)
