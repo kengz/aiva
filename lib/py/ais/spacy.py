0e83507980f9b8e482dca013a6856c79c635337c
kengz <kengzwl@gmail.com>
2016-04-09 21:16:02 -0400

    hasher to skip has input and has no output in msg

---
+++ lib/py/ais/spacy.py
@@ -0,0 +1,22 @@
+# from spacy.en import English
+
+# print(dir())
+
+
+# # spaCy has got:
+# # tokenize, sentence
+# # export to numpy
+# # word vectors
+# # POS tags
+# # semantic dependencies
+# # NER
+
+# # spaCy classes: each has accessible intent
+# # Doc
+# # Token
+# # Span
+# # Lexeme
+# # Vocab
+# # StringStore
+# # 
+# # https://github.com/spacy-io/sense2vec/
