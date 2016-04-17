af825bbedcc54c9acd3f7ddfc5b6bb0d70881202
kengz <kengzwl@gmail.com>
2016-04-17 16:09:39 -0400

    finish slackk_att parsing and interface for gsearch, fix conflict with gkb

--- scripts/google.js
+++ scripts/google.js
@@ -8,12 +8,29 @@ var slackAtt = require('../lib/js/slack_att')
 module.exports = function(robot) {
 
   // menu
-  robot.respond(/google\s*kb$/i, function(res) {
+  robot.respond(/google$/i, function(res) {
+    res.send('`google <query>`')
+  })
+
+  // menu
+  robot.respond(/googlekb$/i, function(res) {
     res.send('`google kb <query>`')
   })
 
   // find out details about users matching the search keyword
-  robot.respond(/google\s*kb\s+(.+)/i, function(res) {
+  robot.respond(/google\s+(.+)/i, function(res) {
+    var query = res.match[1]
+    // call google kg search
+    ai.google.gsearchAsync(query)
+    .then(function(results) {
+      // format and send slack attachments
+      att = slackAtt.gen(res, results, slackAtt.gsearchParser)
+      robot.adapter.customMessage(att)
+    }).catch(console.log)
+  })
+
+  // find out details about users matching the search keyword
+  robot.respond(/googlekb\s+(.+)/i, function(res) {
     var keyword = res.match[1]
     var params = { query: keyword, limit: 5 };
     // call google kg search
