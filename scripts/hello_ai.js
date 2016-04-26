3c39e5987f7e6e5b284252149315a9f56a1afbd6
kengz <kengzwl@gmail.com>
2016-04-26 16:04:09 -0400

    add indico eg

--- scripts/hello_ai.js
+++ scripts/hello_ai.js
@@ -1,10 +1,12 @@
 // dependencies
-// Development process, testing after starting server with "npm run server"
-var client = require('../lib/client.js')
-global.gPass = client.gPass
-
 var genNLP = require('../lib/js/gen_nlp')
 var ai = require('../lib/js/ai')
+
+// // Development process, testing after starting server with "npm run server"
+// var client = require('../lib/client.js')
+// global.gPass = client.gPass
+// var genNLP = require('../lib/js/gen_nlp')
+// var ai = require('../lib/js/ai')
 
 // var input = 'find me flights from New York to London at 9pm tomorrow.'
 // genNLP.parse(input)
@@ -13,9 +15,12 @@ var ai = require('../lib/js/ai')
 // })
 // // parsed NLP results
 
-console.log(ai)
+// ai.indico.sentiment(['indico is so easy to use!', 'Still really easy, yiss'])
+// .then(console.log)
+
 
 // deploy feature: export for bot
+/* istanbul ignore next */
 module.exports = function(robot) {
   // menu
   robot.respond(/nlp\s*$/i, function(res) {
@@ -28,6 +33,16 @@ module.exports = function(robot) {
     genNLP.parse(input)
     .then(function(reply) {
       var output = JSON.stringify(reply, null, 2)
+      res.send(output)
+    })
+  })
+
+  // indico sentiment
+  robot.respond(/sentiment\s*(.+)/i, function(res) {
+    var input = res.match[1];
+    ai.indico.sentiment(input)
+    .then(function(reply) {
+      var output = JSON.stringify(reply)
       res.send(output)
     })
   })
