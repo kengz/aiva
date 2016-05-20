2700dc32c8deb49fc6796f3ade1534091916a3d9
kengz <kengzwl@gmail.com>
2016-05-20 07:32:56 -0400

    use attach to existing window to enter

--- enter.sh
+++ enter.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# A simple docker script to enter into the container
+# A simple docker script to enter the container
 
 if [ "`docker ps | grep aiva-run`" = "" ]; then
-    echo "Making volume"
+    echo "[Making volume]"
     docker run -it -P --rm --name aiva-enter -v `pwd`:/opt/aiva kengz/aiva /bin/bash
 else
-    echo "Steaming volume from aiva-run"
-    docker run -it -P --rm --name aiva-enter --volumes-from=aiva-run kengz/aiva /bin/bash
+    # echo "[Steaming volume from aiva-run]"
+    # docker run -it -P --rm --name aiva-enter --volumes-from=aiva-run kengz/aiva /bin/bash
+    echo "[Attaching to container aiva-run, use Ctrl-p-q to detach]"
+    docker attach aiva-run
 fi
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Run the container
+# Start the container
 
 # todo:
 # move python socket dep to Dockerfile
@@ -7,7 +7,4 @@
 # ahh make one that reconnects and rerun npm run debug, instead of starting all over from scratch install
 # also non-persistent installation when doing 'enter'
 
-docker run --name aiva-run -v `pwd`:/opt/aiva -it -P kengz/aiva /bin/bash -c "cd /opt/aiva && npm run gi && npm run debug"
-# docker run --name aiva-run -v `pwd`:/opt/aiva -d -it -P kengz/aiva tail -f /dev/null
-# docker run --name deepdream-json --volumes-from aiva-run -d ubuntu:14.04 /bin/bash -c "cd /opt/deepdream && ./make_json.sh"
-
+docker run -it -d -P --name aiva-run -v `pwd`:/opt/aiva kengz/aiva /bin/bash -c "cd /opt/aiva && npm run gi && npm run debug"
