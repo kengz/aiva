0a36299d37ab9b8c91c54236ae067593de2812c8
kengz <kengzwl@gmail.com>
2016-05-20 08:53:43 -0400

    note on port streaming failure

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Start the container
 
-# check streaming ports
+# check streaming ports might be due to tcp protocol
 # fileoutput to deepdream folder. test output write to arbit folder
 
 # do mode: debug or start
-docker run -it -d -- expose 4040 -p 4040:4040 -p 7474:7474 --name aiva-run -v `pwd`:/opt/aiva kengz/aiva /bin/bash -c "cd /opt/aiva && npm run gi && npm run debug"
+docker run -it -d -p 4040:4040 -p 7474:7474 --name aiva-run -v `pwd`:/opt/aiva kengz/aiva /bin/bash -c "cd /opt/aiva && npm run gi && npm run debug"
