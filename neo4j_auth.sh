08caf8453c38517d4e2e45f7622ed9b9974c9380
kengz <kengzwl@gmail.com>
2016-05-27 08:29:43 -0400

    add neo4j_auth shell script to auto set password from default from first neo4j install

---
+++ neo4j_auth.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+eval "$(cat .env | sed 's/^/export /')"
+
+if [ "${NEO4J_AUTH:-}" == "none" ]; then
+    echo "dbms.security.auth_enabled=false" >> /etc/neo4j/neo4j.conf
+elif [[ "${NEO4J_AUTH:-}" == neo4j:* ]]; then
+    password="${NEO4J_AUTH#neo4j:}"
+    neo4j start
+
+    end="$((SECONDS+10))"
+    while true; do
+        http_code="$(curl --silent --write-out %{http_code} --user "neo4j:${password}" --output /dev/null http://localhost:7474/db/data/ || true)"
+
+        if [[ "${http_code}" = "200" ]]; then
+            break;
+        fi
+
+        if [[ "${http_code}" = "403" ]]; then
+            echo "changing default password"
+            curl --fail --silent --show-error --user neo4j:neo4j \
+                 --data '{"password": "'"${password}"'"}' \
+                 --header 'Content-Type: application/json' \
+                 http://localhost:7474/user/neo4j/password
+            break;
+        fi
+
+        if [[ "${SECONDS}" -ge "${end}" ]]; then
+            (echo "Neo4j failed to start" && exit 1)
+        fi
+
+        sleep 1
+    done
+
+    neo4j stop
+elif [ -n "${NEO4J_AUTH:-}" ]; then
+    echo "Invalid value for NEO4J_AUTH: '${NEO4J_AUTH}'"
+    exit 1
+fi
