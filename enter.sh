92b82e2588b95f3191c22804df3d15d782e51a24
kengz <kengzwl@gmail.com>
2016-05-27 23:42:32 -0400

    rename enter script msg, use exec for entering with new shell

--- enter.sh
+++ enter.sh
@@ -2,9 +2,11 @@
 # A simple docker script to enter the container
 
 if [ "`docker ps | grep aiva-run`" = "" ]; then
-    echo "[Making volume]"
+    echo "[Start container aiva-enter, un-detached, making volume]"
     docker run -it -p 80:80 -p 4040:4041 -p 7474:7475 --rm --name aiva-enter -v `pwd`:/opt/aiva kengz/aiva /bin/bash
 else
-    echo "[Attaching to container aiva-run, use Ctrl-p-q to detach]"
-    docker attach aiva-run
+    # echo "[Attaching to container aiva-run, use Ctrl-p-q to detach]"
+    # docker attach aiva-run
+    echo "[Attaching to container aiva-run with a new shell]"
+    docker exec -it aiva-run /bin/bash
 fi
--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # Start the container
 
-# check streaming ports might be due to tcp protocol
-# fileoutput to deepdream folder. test output write to arbit folder
-
+echo "[Start container aiva-run, detached]"
 docker run -it -d -p 80:80 -p 4040:4041 -p 7474:7475 --name aiva-run -v `pwd`:/opt/aiva kengz/aiva
