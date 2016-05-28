e01535ba47890ac506a8f9505082ae474b3ae43d
kengz <kengzwl@gmail.com>
2016-05-27 22:47:25 -0400

    finish and tested docker-entrypt for neo4j auth

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 # set from .env
-eval "$(cat .env | sed 's/^/export /')"
+eval "$(cat ../.env | sed 's/^/export /')"
 
 # set your password the first time from the default neo4j:neo4j, or just pass
 if [ "${NEO4J_AUTH:-}" == "none" ]; then
     echo "dbms.security.auth_enabled=false" >> /etc/neo4j/neo4j.conf
 elif [[ "${NEO4J_AUTH:-}" == neo4j:* ]]; then
+    echo "Using NEO4J_AUTH: '$NEO4J_AUTH'"
     password="${NEO4J_AUTH#neo4j:}"
     neo4j start
 
     end="$((SECONDS+10))"
     while true; do
+        echo "posting to Neo4j to auth your NEO4J_AUTH"
         http_code="$(curl --silent --write-out %{http_code} --user "neo4j:${password}" --output /dev/null http://localhost:7474/db/data/ || true)"
 
         if [[ "${http_code}" = "200" ]]; then
+            echo "Neo4j auth successful. You current NEO4J_AUTH is valid"
             break;
         fi
 
@@ -28,7 +31,8 @@ elif [[ "${NEO4J_AUTH:-}" == neo4j:* ]]; then
         fi
 
         if [[ "${SECONDS}" -ge "${end}" ]]; then
-            (echo "Neo4j failed to start" && exit 1)
+            (echo "End of Neo4j auth attempts, Neo4j failed to start" && exit 1)
+            break;
         fi
 
         sleep 1
