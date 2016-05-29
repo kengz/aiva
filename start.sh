2c6043b0435a967a1a5eb8e3da4fb23c586644d5
kengz <kengzwl@gmail.com>
2016-05-29 16:32:42 -0400

    first docker script for npm usage

--- start.sh
+++ start.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-# Start the container
+# Start aiva, use and start Docker container if exists
 
-echo "[Start container aiva-run, detached]"
-docker run -it -d -p 80:80 -p 4040:4041 -p 7474:7475 --name aiva-run -v `pwd`:/opt/aiva kengz/aiva
+container=aiva-run
+
+if [[ "$(docker images -q kengz/aiva:latest 2> /dev/null)" != "" ]]; then
+  echo "[Docker image kengz/aiva pulled, using it. Use Ctrl-p-q to detach]"
+
+  if [[ "$(docker ps -qa --filter name=$container 2> /dev/null)" != "" ]]; then
+    echo "[Docker container $container exists; attaching to it]"
+    docker start $container && docker attach $container
+
+  else
+    echo "[Creating new Docker container $container, detached]"
+    docker run -it -p 4040:4041 -p 7474:7475 --name $container -v `pwd`:/opt/aiva kengz/aiva
+  fi
+
+else # not using Docker
+  echo "[Start on local machine, not Docker]"
+  npm start
+fi;
+
+# this script always attach to the main supervisord bash session
+# add note to use Ctrl-p-q
