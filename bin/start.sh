c7bef684cd0bd3e25381d79965e1f8a0471438f1
kengz <kengzwl@gmail.com>
2016-09-25 12:16:22 -0400

    move copy env file script to start

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,34 @@
 # Start aiva, use and start Docker container if exists
 # this bash session is for use with supervisord. For new bash sessions to the same container, use enter.sh
 
+
+# copy keys file if not already exist
+AIVA_DIR=`pwd`
+BIN_DIR="$AIVA_DIR/bin"
+
+env_count=$(find $AIVA_DIR -name ".env*" -maxdepth 1 -type f | wc -l)
+example_env_count=$(find $AIVA_DIR -name ".env-example" -maxdepth 1 -type f | wc -l)
+if [[ $env_count -eq 1 && $example_env_count -eq 1 ]]; then (
+  cp $AIVA_DIR/.env-example $AIVA_DIR/.env
+  echo "Created .env"
+  );
+  else (
+  echo "Not creating .env since it already exists"
+  )
+fi
+
+keys_count=$(find $BIN_DIR -name ".keys-*" -maxdepth 1 -type f | wc -l)
+example_keys_count=$(find $BIN_DIR -name ".keys-example" -maxdepth 1 -type f | wc -l)
+if [[ $keys_count -eq 1 && $example_keys_count -eq 1 ]]; then (
+  cp $BIN_DIR/.keys-example $BIN_DIR/.keys-aiva
+  cp $BIN_DIR/.keys-example $BIN_DIR/.keys-aivadev
+  echo "Created bin/.keys-aiva, bin/.keys-aivadev"
+  );
+  else (
+  echo "Not creating bin/.keys-aiva, bin/.keys-aivadev since keys already exist"
+  )
+fi
+
 # set from .env
 eval "$(cat .env | sed 's/^/export /')"
 
