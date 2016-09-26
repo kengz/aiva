05d53661df5757c22e1e7cd60ff5267c208eacd7
kengz <kengzwl@gmail.com>
2016-09-26 00:05:26 -0400

    refactor test/common

--- test/common.js
+++ test/common.js
@@ -1,6 +1,4 @@
-// dependencies
 // common setups for tests, run before tests
-// var env = require('node-env-file');
 const config = require('config')
 const Log = require('log')
 const path = require('path')
@@ -14,34 +12,31 @@ try {
   setEnv()
   process.env.NODE_ENV = 'development'
   process.env.PORT = 9090
-  log.info("Test is using PORT:", process.env.PORT)
+  log.info(`Test is using PORT: ${process.env.PORT}`)
 } catch (e) {
   log.error(JSON.stringify(e, null, 2))
-  if (process.env.CI) {
-    log.info("Using config file from CI if exist.")
-  } else {
-    log.error("No config and not in CI, please provide your config file.")
-    process.exit(1)
-  }
+  log.error("No config and not in CI, please provide your config file.")
+  process.exit(1)
 }
 
 // unit test global dependencies
 require(path.join(__dirname, '..', 'scripts', '_init'))
 global.DEFAULT_ROOM = "bot-test" // set for test
-global.Promise.config({ warnings: false });
-// chai assertation library
-global.chai = require('chai')
+global.Promise.config({ warnings: false })
+
+global.chai = require('chai') // chai assertation library
 global.chaiAsPromised = require("chai-as-promised")
 chai.use(chaiAsPromised)
-global.should = chai.should();
-// sinon spy/stub library
-global.sinon = require('sinon');
-global.Helper = require('hubot-test-helper');
+global.should = chai.should()
+global.sinon = require('sinon') // sinon spy/stub library
+global.Helper = require('hubot-test-helper')
+
 // set the hubot say handlers for unit tests: send reply to room
 global.say = function(room, name, key) {
   key = key || 'output'
   return _.flow(_.partial(_.get, _, key), _.bind(room.user.say, room, name))
 }
+
 // Promise.delay, with adjusted time factors. for use with yield
 global.delayer = function(factor) {
   factor = factor || 1
