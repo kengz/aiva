791c5d9a8fe60ae9e3b8e4b6b5f7c670db233e1d
kengz <kengzwl@gmail.com>
2016-09-25 22:54:46 -0400

    improve copy config script

--- bin/copy-config.sh
+++ bin/copy-config.sh
@@ -2,32 +2,16 @@
 
 # copy keys file if not already exist
 CONFIG_DIR=`pwd`/config
-EG_CONFIG_FILE="$CONFIG_DIR/example-default.json"
-CONFIG_FILE="$CONFIG_DIR/default.json"
+EXAMPLE_CONFIG="$CONFIG_DIR/example-default.json"
+DEV_CONFIG="$CONFIG_DIR/default.json"
+PROD_CONFIG="$CONFIG_DIR/production.json"
 
-if [ ! -e "$CONFIG_FILE" ]; then
-  cp $EG_CONFIG_FILE $CONFIG_FILE
-  echo "Created $CONFIG_FILE"
+if [ ! -e "$PROD_CONFIG" ]; then
+  cp $EXAMPLE_CONFIG $PROD_CONFIG
+  echo "Created $PROD_CONFIG"
 fi
-# env_count=$(find $AIVA_DIR -name ".env*" -maxdepth 1 -type f | wc -l)
-# example_env_count=$(find $AIVA_DIR -name ".env-example" -maxdepth 1 -type f | wc -l)
-# if [[ $env_count -eq 1 && $example_env_count -eq 1 ]]; then (
-#   cp $AIVA_DIR/.env-example $AIVA_DIR/.env
-#   echo "Created .env"
-#   );
-#   else (
-#   echo "Not creating .env since it already exists"
-#   )
-# fi
 
-# keys_count=$(find $BIN_DIR -name ".keys-*" -maxdepth 1 -type f | wc -l)
-# example_keys_count=$(find $BIN_DIR -name ".keys-example" -maxdepth 1 -type f | wc -l)
-# if [[ $keys_count -eq 1 && $example_keys_count -eq 1 ]]; then (
-#   cp $BIN_DIR/.keys-example $BIN_DIR/.keys-aiva
-#   cp $BIN_DIR/.keys-example $BIN_DIR/.keys-aivadev
-#   echo "Created bin/.keys-aiva, bin/.keys-aivadev"
-#   );
-#   else (
-#   echo "Not creating bin/.keys-aiva, bin/.keys-aivadev since keys already exist"
-#   )
-# fi
+if [ ! -e "$PROD_CONFIG" ]; then
+  cp $EXAMPLE_CONFIG $PROD_CONFIG
+  echo "Created $PROD_CONFIG"
+fi
--- bin/start.sh
+++ bin/start.sh
@@ -2,43 +2,16 @@
 # Start aiva, use and start Docker container if exists
 # this bash session is for use with supervisord. For new bash sessions to the same container, use enter.sh
 
-
 # copy keys file if not already exist
-AIVA_DIR=`pwd`
-BIN_DIR="$AIVA_DIR/bin"
+BIN_DIR=`pwd`/bin
+$BIN_DIR/copy-config.sh
 
-env_count=$(find $AIVA_DIR -name ".env*" -maxdepth 1 -type f | wc -l)
-example_env_count=$(find $AIVA_DIR -name ".env-example" -maxdepth 1 -type f | wc -l)
-if [[ $env_count -eq 1 && $example_env_count -eq 1 ]]; then (
-  cp $AIVA_DIR/.env-example $AIVA_DIR/.env
-  echo "Created .env"
-  );
-  else (
-  echo "Not creating .env since it already exists"
-  )
-fi
-
-keys_count=$(find $BIN_DIR -name ".keys-*" -maxdepth 1 -type f | wc -l)
-example_keys_count=$(find $BIN_DIR -name ".keys-example" -maxdepth 1 -type f | wc -l)
-if [[ $keys_count -eq 1 && $example_keys_count -eq 1 ]]; then (
-  cp $BIN_DIR/.keys-example $BIN_DIR/.keys-aiva
-  cp $BIN_DIR/.keys-example $BIN_DIR/.keys-aivadev
-  echo "Created bin/.keys-aiva, bin/.keys-aivadev"
-  );
-  else (
-  echo "Not creating bin/.keys-aiva, bin/.keys-aivadev since keys already exist"
-  )
-fi
-
-# set from .env
-eval "$(cat .env | sed 's/^/export /')"
-
-if [[ $CLOUDDREAM_PATH && -d ${HOME}/clouddream ]]; then
-  echo "Using AIVA with Deepdream: make sure you start ${HOME}/clouddream"
-  CLOUDDREAM_LINK_VOL="-v ${HOME}/clouddream:${CLOUDDREAM_PATH}"
-else
-  CLOUDDREAM_LINK_VOL=""
-fi
+# if [[ $CLOUDDREAM_PATH && -d ${HOME}/clouddream ]]; then
+#   echo "Using AIVA with Deepdream: make sure you start ${HOME}/clouddream"
+#   CLOUDDREAM_LINK_VOL="-v ${HOME}/clouddream:${CLOUDDREAM_PATH}"
+# else
+#   CLOUDDREAM_LINK_VOL=""
+# fi
 
 if [[ $1 && $1=='production' ]]; then
   container=aiva-production
