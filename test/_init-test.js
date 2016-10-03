6a3eb57097ebd3896c4c552b379d05eee7ebecb7
kengz <kengzwl@gmail.com>
2016-10-02 23:24:01 -0400

    remove authdb seq from ci, leave migration

--- test/_init-test.js
+++ test/_init-test.js
@@ -6,7 +6,7 @@ const aiva = require(path.join(__dirname, 'common'))
 before(() => {
   return co(function*() {
     yield aiva.start()
-    
+
     global.log.info(`Running in ${process.env.NODE_ENV} mode`)
     global.log.info(`Create rooms, init brain for tests`)
     var helper = new Helper(path.join(__dirname, '..', 'scripts')) // emulate full hubot init
@@ -16,7 +16,6 @@ before(() => {
 
     // set the brain to test/asset.js's
     _.set(this.room.robot, 'brain.data.users', users)
-    console.log('global ioPromise', global.ioPromise)
     yield global.ioPromise
   })
 })
