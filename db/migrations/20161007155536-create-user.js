b36bd433c17024c64b68815676b7a689828b3afe
kengz <kengzwl@gmail.com>
2016-10-07 14:49:09 -0400

    add user db in chatlog

---
+++ db/migrations/20161007155536-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      adapter: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userid: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      profile: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  }
+};
