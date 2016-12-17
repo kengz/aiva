b7e5cb6dc78da2682d3ed40827be88ff3a3f6f69
kengz <kengzwl@gmail.com>
2016-12-17 13:46:54 -0500

    get basic receive emulation working

--- scripts/emulate-receive.js
+++ scripts/emulate-receive.js
@@ -3,11 +3,13 @@
 const _ = require('lomath')
 const path = require('path')
 const { User } = require(path.join(__dirname, '..', 'db', 'models', 'index'))
+const { TextMessage } = require('hubot')
 
 /* istanbul ignore next */
 module.exports = (robot) => {
   /* istanbul ignore next */
-  robot.respond(/.*/, (res) => {
+  robot.respond(/emulate.*/, (res) => {
+
     var adapter = process.env.ADAPTER
     var userid = _.toString(_.get(res.envelope, 'user.id'))
     User.find({
@@ -15,7 +17,10 @@ module.exports = (robot) => {
       })
       .then((user) => {
         var envelope = JSON.parse(user.envelope)
-        robot.send(envelope, 'Emulate message ')
+        robot.send(envelope, 'Emulate message')
+
+        message = new TextMessage(envelope.user, `${robot.name} ping`, '0')
+        robot.receive(message)
       })
   })
 }
