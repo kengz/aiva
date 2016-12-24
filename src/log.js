8e29106a9da4ed59dbe475b5f7a86b8a86f4aa6a
kengz <kengzwl@gmail.com>
2016-12-24 14:25:04 -0500

    fix winston format str len

--- src/log.js
+++ src/log.js
@@ -5,9 +5,11 @@ const logLevel = process.env.npm_config_debug ? 'debug' : 'info'
 const log = global.log || new (winston.Logger)({
   level: logLevel,
   transports: [new (winston.transports.Console)({
-    formatter: options =>
-      /* istanbul ignore next */
-      `[${new Date()}] ${winston.config.colorize(options.level, options.level.toUpperCase())} ${options.message || ''}`,
+    /* istanbul ignore next */
+    formatter: (options) => {
+      const formatStr = `[${new Date()}] ${winston.config.colorize(options.level, options.level.toUpperCase())} ${options.message || ''}`
+      return formatStr
+    },
   })],
 })
 global.log = log
